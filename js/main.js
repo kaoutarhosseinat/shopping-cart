0667ccdf05b77bbbdf0e85617e59ac9937b7ade9
kaoutar hosseinat <kaoutarhosseinat@gmail.com>
2022-11-14 20:03:59 +0100

    1st

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+
+
+let cartIcon =document.querySelector("#cart-icon")
+let cart =document.querySelector(".cart")
+let closeCart =document.querySelector("#close-cart")
+
+cartIcon.onclick = () =>{
+    cart.classList.add("active")
+};
+closeCart.onclick = () =>{
+    cart.classList.remove("active")
+};
+
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready )
+}else{
+    ready();
+}
+function ready(){
+    let removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(
+        removeCartButtons
+    )
+    for (let i = 0; i < removeCartButtons.length; i++){
+        let button =removeCartButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+function removeCartItem(event){
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updatetotal();
+
+}
+function updatetotal(){
+    let cartContent = document.getElementsByClassName("cart-content")[0];
+    let cartBoxes = cartContent.getElementsByClassName("cart-box");
+    total = 0;
+    for (let i = 0; i < cartBoxes.length; i++){
+        let cartBox = cartBoxes[i];
+        let priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+        let price = parseFloat(priceElement.innerText.replace("DZD",""));
+        let quantity = quantityElement.value;
+        total = total + price * quantity;
+        document.getElementsByClassName("total-price")[0].innerText ="DZD" + total;
+    }
+}
